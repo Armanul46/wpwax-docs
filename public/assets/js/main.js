dc23967d49d39e6a365136184c185a34b4c58c2c
shamimsaj <shamimeforb@gmail.com>
2020-04-30 12:57:35 +0600

    fixed issue

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,11 +1,10 @@
 (function($) {
     jQuery(document).ready(function($) {
         $(".wpwax-single-docs .left-sidebar ul").slideUp();
-        $(".wpwax-single-docs .left-sidebar:first-child h4").addClass("active");
-        if($(".wpwax-single-docs .left-sidebar:first-child h4").hasClass("active")){
-            $(".wpwax-single-docs .left-sidebar:first-child ul").slideDown();
-        }
         $(".wpwax-single-docs .left-sidebar h4").each(function (i, e) {
+            if($(e).hasClass("active")){
+                $(e).next().slideDown();
+            }
             $(e).on("click", function () {
                 $(".wpwax-single-docs .left-sidebar").siblings().children("h4").removeClass("active");
                 $(".wpwax-single-docs .left-sidebar").siblings().children("ul").slideUp();
